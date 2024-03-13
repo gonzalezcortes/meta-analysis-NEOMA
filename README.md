8aa456cc51a9851f88c2978ceb15999219e129de
Daniel González Cortés <47788332+gonzalezcortes@users.noreply.github.com>
2024-03-13 09:51:23 +0100

    Meta_1

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Meta Analysis NEOMA
 
 Repository for meta analysis at NEOMA, March 2024.
+
+
+## Structure
+	* meta_1: contains the data and the R script for the meta analysis
+
+The data contains results from 46 studies, including 89 effect sizes (r), on the specificity of future thinking in depression (Gamble et al. 2019).
+
+Data sources:
+- https://osf.io/5wjb2/
--- meta_1.R
+++ meta_1.R
@@ -1,1 +1,18 @@
 #Meta-analysis workshop
+#libraries
+
+#call psychmeta library
+library(psychmeta)
+
+# Example dataset
+df <- data.frame(
+  study = c("Study 1", "Study 2", "Study 3"),
+  effect_size = c(0.2, 0.3, 0.25),
+  sample_size = c(100, 150, 120)
+)
+
+# Conducting the meta-analysis
+meta_analysis_result <- ma_r(r = df$effect_size, n = df$sample_size, data = df)
+
+# Viewing the results
+print(meta_analysis_result)
